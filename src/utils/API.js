c6893e5a543ff9f272d5d081fc329f73179b71f4
Avery Brown-Gathings <72925420+averyjbrown2@users.noreply.github.com>
2021-01-12 10:23:16 -0500

    removed api key from api.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 const BASEURL = "https://randomuser.me/api/?inc=";
-const APIKEY = "";
+
 
 export default {
   search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
+    return axios.get(BASEURL + query);
   }
 };
